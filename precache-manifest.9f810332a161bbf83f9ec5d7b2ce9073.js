c541403e0f6a038af42c8ae8211740471b829ed5
Loki Coyote <lokkju@gmail.com>
2020-01-09 02:29:38 -0800

    Updates

---
+++ precache-manifest.9f810332a161bbf83f9ec5d7b2ce9073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28b78d00edc7553a65c7367c8d98ae5",
+    "url": "/helium-hotspot-config/index.html"
+  },
+  {
+    "revision": "4e7f09e3a03ac0d0741c",
+    "url": "/helium-hotspot-config/static/css/main.8657f287.chunk.css"
+  },
+  {
+    "revision": "022880dd727c32618f2d",
+    "url": "/helium-hotspot-config/static/js/2.5b4db9e1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/helium-hotspot-config/static/js/2.5b4db9e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e7f09e3a03ac0d0741c",
+    "url": "/helium-hotspot-config/static/js/main.810e8604.chunk.js"
+  },
+  {
+    "revision": "4644ea0092c8ef1676f2",
+    "url": "/helium-hotspot-config/static/js/runtime-main.08fd18f6.js"
+  }
+]);
